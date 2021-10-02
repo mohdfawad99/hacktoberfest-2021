58b08f9a9843516b8838752acdb0bf7088573b04
Divya Singh <67798886+divya1467@users.noreply.github.com>
2021-10-02 09:37:31 +0530

    Update Tower_of_hanoi.java

--- Java Programs/Tower_of_hanoi.java
+++ Java Programs/Tower_of_hanoi.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class TowerOfHanoi {
+public class Tower_of_hanoi {
     public static void main(String args[])
     {
         Scanner in = new Scanner(System.in);
